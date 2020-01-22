19775cc2d02322e23011c1757d57c3ee3bd25169
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2020-01-22 16:14:05 +0100

    Update commands helm file

--- commands.sh
+++ commands.sh
@@ -1,19 +1,22 @@
 # metronome
 
-metronome play 120 4 4  -o stdout
+metronome play 120 4 4 -o stdout
 
-metronome play 120 4 4  -o audio
+metronome play 120 4 4 -o audio
 
 
 # controller
 
-
---disable-controller
-
---storage-path /Users/apinnecke/src/github.com/OneMoreWord/sac-data/storage
+controller server --storage-path ~/src/github.com/aklinkert/godays-lighting-talk/demo-storage --disable-controller
 
 # defer
 
 time go run code/counter.go
 
 time go run code/counter.go -defer
+
+# DMX timing
+
+go run code/music_fps.go
+
+go run code/channels_fps.go
